a131d3a219ca95ce44cf03a73169da2804df0906
zaidahmed332675 <56406369+zaidahmed332675@users.noreply.github.com>
2020-06-07 23:03:15 +0500

    FINAL PUSH

--- chapter1Solved/app.js
+++ chapter1Solved/app.js
@@ -1,10 +1,33 @@
+/* 1. Write a script to greet your website visitor using JS alert
+box. */
 alert("WELCOME TO MAKHISOL");
+
+/* 2. Write a script to display following message on your webpage: */
 alert("Error! please Enter a Valid Password");
+
+/* 3. Write a script to display following message on your web
+page: (Hint : Use line break) */
 alert("Welcome To Js Land...\r\nHappy Coding!");
+
+/* 4. Write a script to display following messages in sequence: */
 alert("Welcome To Js Land");
 alert("Happy Coding!");
+
+/* 5. Generate the following message through browser’s
+developer console: */
 alert("Using Alert In New Project!");
+
+/* 7. Practice placement of <script></script> element in
+following sections of your project in exercise 6: */
+
+// a. Head
 alert("Alert From Head");
+
+// b. Body (before your page’s HTML)
 alert("Alert BEFORE YOUR HTML PAGE");
+
+// c. Body (inside your page’s HTML)
 alert("Alert INSIDE YOUR HTML PAGE");
+
+// d. Body (after your page’s HTML)
 alert("Alert AFTER YOUR HTML PAGE");
--- chapter6-9Solved/app.js
+++ chapter6-9Solved/app.js
@@ -2,23 +2,23 @@
 required arithmetic to display the following result in your
 browser. */
 
-    document.write("<h1>Result</h1><br>");
+document.write("<h1>Result</h1><br>");
 
-    var randomNum = +prompt("Enter A Number");
+var randomNum = +prompt("Enter A Number");
 
-    document.write("The Value Of Variable Is: "+ randomNum+"<br><br>");
+document.write("The Value Of Variable Is: "+ randomNum+"<br><br>");
 
-    document.write("The Value Of ++variable Is: "+ (++randomNum) +"<br>");
-    document.write("Now The Value Of Variable Is: "+ randomNum +"<br><br>");
+document.write("The Value Of ++variable Is: "+ (++randomNum) +"<br>");
+document.write("Now The Value Of Variable Is: "+ randomNum +"<br><br>");
 
-    document.write("The Value Of variable++ Is: "+ (randomNum++) +"<br>");
-    document.write("Now The Value Of Variable Is: "+ randomNum +"<br><br>");
+document.write("The Value Of variable++ Is: "+ (randomNum++) +"<br>");
+document.write("Now The Value Of Variable Is: "+ randomNum +"<br><br>");
 
-    document.write("The Value Of --variable Is: "+ (--randomNum) +"<br>");
-    document.write("Now The Value Of Variable Is: "+ randomNum +"<br><br>");
+document.write("The Value Of --variable Is: "+ (--randomNum) +"<br>");
+document.write("Now The Value Of Variable Is: "+ randomNum +"<br><br>");
 
-    document.write("The Value Of variable-- Is: "+ (randomNum--) +"<br>");
-    document.write("Now The Value Of Variable Is: "+ randomNum +"<br><br>");
+document.write("The Value Of variable-- Is: "+ (randomNum--) +"<br>");
+document.write("Now The Value Of Variable Is: "+ randomNum +"<br><br>");
 
 /* 2. What will be the output in variables a, b & result after
 execution of the following script: */
