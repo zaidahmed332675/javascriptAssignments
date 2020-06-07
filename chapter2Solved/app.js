f7c272d7500f08aaf927c840e0fe1464e4ff1624
zaidahmed332675 <56406369+zaidahmed332675@users.noreply.github.com>
2020-06-07 23:04:58 +0500

    FINAL PUSH

--- chapter2Solved/app.js
+++ chapter2Solved/app.js
@@ -1,5 +1,3 @@
-
-
 // 1. Declare a variable called username.
 var username;
 
@@ -8,13 +6,14 @@ that represents your Full Name. */
 var myName = "ZAID BIN AHMED MAKHARISH";
 
 // 3. Write script to.
-    
 // a) Declare a JS variable, titled message.
-    var message;
-    // b) Assign “Hello World” to variable message
-    message = "Hello World";
-    // c) Display the message in alert box.
-    alert(message);
+var message;
+
+// b) Assign “Hello World” to variable message
+message = "Hello World";
+
+// c) Display the message in alert box.
+alert(message);
 
 /* 4. Write a script to save student’s bio data in JS variables and
 show the data in alert boxes */
@@ -28,6 +27,7 @@ variable: */
 
 
 
+
 /* 6- Declare a variable called email and assign to it a string that
 represents your Email Address(e.g. example@example.com).
 Show the blow mentioned message in an alert box.(Hint: use
@@ -35,7 +35,6 @@ string concatenation) */
 
 var email = "zaidahmed332675@gmail.com";
 alert("My Email Address Is " + email);
-
 
 /* 7. Declare a variable called book & give it the value “A
 smarter way to learn JavaScript”. Display the following
